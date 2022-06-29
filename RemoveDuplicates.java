b662228b23f86fb432e593a514616ced132e9e63
LogeswariMohan <107361360+LogeswariMohan@users.noreply.github.com>
2022-06-29 17:11:33 +0530

    Update RemoveDuplicates.java

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -7,14 +7,12 @@ public class RemoveDuplicates {
 
 	public static void main(String[] args) {
 		
-		String text = "We learn java basics as part of java sessions in java week1";		
-
-		int count=0;
+		String text = "We learn java basics as part of java sessions in java week1";	
 	  
 		String[] words = text.split(" ");
 		int len = words.length;
-		
-			Set<String> removeset=new LinkedHashSet<String>();
+			
+			Set<String> removeset=new LinkedHashSet<String>();//we are taking Set, as it  does not allows duplicate
 			for (String var : words) {
 				removeset.add(var);
 				
