f5b285e95bd9ec361b4ccef0f4fd553a5013b1d5
LogeswariMohan <107361360+LogeswariMohan@users.noreply.github.com>
2022-06-29 17:07:46 +0530

    Update RemoveDuplicates.java

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -8,8 +8,7 @@ public class RemoveDuplicates {
 	public static void main(String[] args) {
 		
 		String text = "We learn java basics as part of java sessions in java week1";		
-	 	
-		String str="";
+
 		int count=0;
 	  
 		String[] words = text.split(" ");
