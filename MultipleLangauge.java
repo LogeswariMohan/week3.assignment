806cbb998c37e5e116c3146832f0e86aad61189f
LogeswariMohan <107361360+LogeswariMohan@users.noreply.github.com>
2022-07-01 17:58:47 +0530

    Add files via upload

---
+++ MultipleLangauge.java
@@ -0,0 +1,10 @@
+package week3.assignmentss;
+
+public abstract class MultipleLangauge {
+	
+	public void python() {
+		System.out.println("Python is a language");
+	}
+	public abstract void ruby();
+
+}
